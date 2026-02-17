709c6402f0e6347982a27cf4e9f000829c9bc117
Yukti Mehta <mehtayukti317@gmail.com>
2026-02-17 16:51:38 +0530

    change port

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use('/api/application',applicationroutes)
 app.use('/api/inquery',inqueryroutes)
 app.use('/api/dashboard',dashboardroutes)
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5001
 app.get("/", (req, res) => {
     res.send("Node js code deployedd...")
 })
