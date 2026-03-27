8ca6e43e3b5fcc24e3282a50de9e1bf6ca2ed9e2
tdtechsolutions2019-ship-it <tdtechsolutions2019@gmail.com>
2026-03-27 16:22:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ import path from "path";
 const app = express()
 app.use(express.json())
 app.use(cors({
-    origin: "http://cayps.co/",
+    origin: "https://cayps.co/",
     // origin: "http://localhost:3000",
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Cache-Control', 'Expires', 'Pragma'],
